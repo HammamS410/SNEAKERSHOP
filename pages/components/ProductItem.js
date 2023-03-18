c6a9b664db06a970952df07310e52e47f433c5b3
Shahifah Hammam <hammam6124@gmail.com>
2023-03-18 12:07:24 +0700

    Load Product from MongoDB

--- pages/components/ProductItem.js
+++ pages/components/ProductItem.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export default function ProductItem({ product }) {
+export default function ProductItem({ product, addToCartHandler }) {
   return (
     <div className="card">
       <Link href={`/product/${product.slug}`}>
@@ -15,7 +15,7 @@ export default function ProductItem({ product }) {
         </Link>
         <p className="text-sm">{product.brand}</p>
         <p className="text-lg font-semibold">$ {product.price}</p>
-        <button className="primary-button" type="button">
+        <button className="primary-button" type="button" onClick={() => addToCartHandler(product)}>
           Add to Cart
         </button>
       </div>
