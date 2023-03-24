f712eafb2bc110be9b0f60bd0bdf578b9dac3013
Shahifah Hammam <hammam6124@gmail.com>
2023-03-24 10:49:46 +0700

    Admin Create and Delete product

--- pages/api/admin/products/index.js
+++ pages/api/admin/products/index.js
@@ -7,12 +7,33 @@ const handler = async (req, res) => {
   if (!session || !session.user.isAdmin) {
     return res.status(401).send({ message: "admin signin required" });
   }
-  //   const { user } = session;
+  // const { user } = session;
   if (req.method === "GET") {
     return getHandler(req, res);
+  } else if (req.method === "POST") {
+    return postHandler(req, res);
   } else {
     return res.status(400).send({ message: "Method not allowed" });
   }
+};
+
+const postHandler = async (req, res) => {
+  await db.connect();
+  const newProduct = Product({
+    name: "sample name",
+    slug: "sample-name-" + Math.random(),
+    category: "sample category",
+    image: "/images/Adidas Matchcourt 1.jpg",
+    price: 0,
+    brand: "sample brand",
+    rating: 0,
+    numReviews: 0,
+    countInstock: 0,
+    discount: 5,
+    description: "sample description",
+  });
+  await db.disconnect();
+  res.send({ message: "Product successfully created" }, newProduct);
 };
 
 const getHandler = async (req, res) => {
